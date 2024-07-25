b1d2e1e16cbb921225697f6acaa48dca9ca0b93a
vctr-me <166289992+vctr-me@users.noreply.github.com>
2024-07-25 20:15:29 +0300

    Update xx_QAstart.R

--- xx_QAstart.R
+++ xx_QAstart.R
@@ -1,4 +1,17 @@
-library(tidyverse)
+# Here is an example of grabbing a rwl from two rds files. 
+# It shows how we can extract a rwl object and its meta info
+
 library(dplR)
 rwls <- readRDS("Rdatafiles/rwls.rds")
 rwls_meta <- readRDS("Rdatafiles/rwls_meta.rds")
+
+nrow(rwls_meta)
+length(rwls)
+
+# the first study
+rwls_meta[1,]
+aRWL <- rwls[[1]]
+
+rwl.report(aRWL)
+summary(aRWL)
+plot(aRWL)
